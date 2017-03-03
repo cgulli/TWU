24071c67a7c9b5fa4d9ed9dbebbaffe773e09098
Caitlin Gulliford <caitlingulliford@Caitlins-MacBook-Pro.local>
2017-03-03 15:53:31 +0000

    first triangle task

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TriangleExercises first = new TriangleExercises();
+        first.asterisk();
+    }
+}
---
+++ src/com/company/TriangleExercises.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class TriangleExercises {
+
+    String star = "*";
+
+    void asterisk(){
+        System.out.println(star);
+    }
+
+
+
+
+}
