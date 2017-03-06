4247d1beb9792cb9082251be1a17ecb17cd0e620
cgulli <caitlin.gulliford@gmail.com>
2017-03-06 12:07:56 +0000

    completed fizz buzz exercise

--- FizzBuzz/src/com/thoughtworks/FizzBuzz.java
+++ FizzBuzz/src/com/thoughtworks/FizzBuzz.java
@@ -4,4 +4,21 @@ package com.thoughtworks;
  * Created by caitlingulliford on 06/03/2017.
  */
 public class FizzBuzz {
+
+    void fizzBuzzMethod (){
+
+        for(int i = 1; i <= 100; i ++){
+            if ((i%3 == 0) && (i%5 == 0))
+                System.out.println("FizzBuzz");
+
+            else if(i%3 == 0)
+                System.out.println("Fizz");
+            else if (i%5 == 0)
+                System.out.println("Buzz");
+
+            else
+                System.out.println(i);
+        }
+    }
+
 }
--- FizzBuzz/src/com/thoughtworks/Main.java
+++ FizzBuzz/src/com/thoughtworks/Main.java
@@ -3,6 +3,8 @@ package com.thoughtworks;
 public class Main {
 
     public static void main(String[] args) {
+        FizzBuzz gogogadget = new FizzBuzz();
+        gogogadget.fizzBuzzMethod();
 
     }
 }
