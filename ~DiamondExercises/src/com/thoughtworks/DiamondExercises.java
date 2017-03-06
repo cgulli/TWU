ee91b4dec42b901831df3826d3066a8ca3e0fb8a
cgulli <caitlin.gulliford@gmail.com>
2017-03-06 11:12:51 +0000

    printing a diamond exercise

--- ~DiamondExercises/src/com/thoughtworks/DiamondExercises.java
+++ ~DiamondExercises/src/com/thoughtworks/DiamondExercises.java
@@ -5,20 +5,50 @@ package com.thoughtworks;
  */
 public class DiamondExercises {
 
-    String star = "*";
+    String ast = "*";
 
-    void isotriangle(int n) {
-        System.out.println("1st Diamond Task: Isosceles Trianlge");
+    void isoTriangle(int n) {
+
+//        for (int i = 1; i <= n; i += 2) {
+//            for (int k = 0; k < (4 - i / 2); k ++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j < i; j ++) {
+//                System.out.print(ast);
+//            }
+//            System.out.println("");
+
+
         for (int i = 1;i <= n; i ++){
 
             for (int k = 1; k <= n-i; k ++)
                 System.out.print(" ");
 
             for (int j = 1; j <= 2 * i - 1; j ++)
-                System.out.print(star);
+                System.out.print(ast);
 
             System.out.println("");
 
+        }
+    }
+
+    void printDiamond(int n){
+        isoTriangle(n);
+        printLowerTriangle(n - 1);
+    }
+
+    void printLowerTriangle(int n) {
+        int starCounter = n;
+
+        for (int k = 1; k <= n; k ++){
+            for (int j =1; j <= k; j ++)
+                System.out.print(" ");
+
+            for (int star = 1; star <= 2*starCounter - 1; star ++) {
+                System.out.print(ast);
+            }
+            starCounter --;
+            System.out.println("");
         }
     }
 }
--- ~DiamondExercises/src/com/thoughtworks/Main.java
+++ ~DiamondExercises/src/com/thoughtworks/Main.java
@@ -3,8 +3,12 @@ package com.thoughtworks;
 public class Main {
 
     public static void main(String[] args) {
+    System.out.println("1st Diamond Task: Isosceles Triangle");
 	DiamondExercises first = new DiamondExercises();
-	first.isotriangle(3);
+	first.isoTriangle(3);
+    System.out.println("2nd Diamond Task: Print a Diamond");
+	DiamondExercises second = new DiamondExercises();
+	second.printDiamond(3);
 
     }
 }
