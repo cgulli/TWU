6c4f14be0ae4317147934c40b4389cd0b8ff95cc
cgulli <caitlin.gulliford@gmail.com>
2017-03-06 10:23:14 +0000

    iso triangle exercise

--- ~DiamondExercises/src/com/thoughtworks/DiamondExercises.java
+++ ~DiamondExercises/src/com/thoughtworks/DiamondExercises.java
@@ -9,14 +9,14 @@ public class DiamondExercises {
 
     void isotriangle(int n) {
         System.out.println("1st Diamond Task: Isosceles Trianlge");
+        for (int i = 1;i <= n; i ++){
 
-        for (int i = 1; i <= n; i += 2) {
-            for (int k = 0; k < (4 - i / 2); k++) {
+            for (int k = 1; k <= n-i; k ++)
                 System.out.print(" ");
-            }
-            for (int j = 0; j < i; j++) {
+
+            for (int j = 1; j <= 2 * i - 1; j ++)
                 System.out.print(star);
-            }
+
             System.out.println("");
 
         }
--- ~DiamondExercises/src/com/thoughtworks/Main.java
+++ ~DiamondExercises/src/com/thoughtworks/Main.java
@@ -3,6 +3,8 @@ package com.thoughtworks;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	DiamondExercises first = new DiamondExercises();
+	first.isotriangle(3);
+
     }
 }
