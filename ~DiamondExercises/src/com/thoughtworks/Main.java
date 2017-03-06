dac6bf798e9d7662a35a510a5ecebfe09882236d
Tito Sarrionandia <r@sarrionandia.com>
2017-03-06 20:49:34 +0000

    Tito - Refactor shapes

--- ~DiamondExercises/src/com/thoughtworks/Main.java
+++ ~DiamondExercises/src/com/thoughtworks/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1st Diamond Task: Isosceles Triangle");
 	DiamondExercises first = new DiamondExercises();
-	first.isoTriangle(3);
+	System.out.println(first.isoTriangle(3));
         System.out.println("2nd Diamond Task: Print a Diamond");
 	DiamondExercises second = new DiamondExercises();
-	second.printDiamond(3);
+        System.out.println(second.printDiamond(3));
         System.out.println("3rd Diamond Task: Diamond with name");
     DiamondExercises third = new DiamondExercises();
-    third.nameDiamond(3, "Caitlin");
+    System.out.println(third.nameDiamond(3, "Caitlin"));
 
 
     }
